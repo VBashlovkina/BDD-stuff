87512e31e09c68190e7f596ccaf0634b54c9faa4
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2014-10-30 10:42:30 -0500

    initial commit

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,62 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|  Movie.create!(movie)
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  page.body.should match (/#{e1}.*#{e2}/m)
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/,\s+/).each do |r| 
+    if ! uncheck
+     check("ratings_#{r}")
+  else 
+      uncheck("ratings_#{r}")
+    end
+  end
+end
+
+
+When /I check all ratings/ do
+ Movie.all.each do |movie|
+    check("ratings_#{movie.rating}")
+  end
+end
+
+
+Then /I should see all the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+  Movie.all.each do |movie|
+    page.body.should match(/#{movie.title}/)
+  end 
+end
+ 
+
+Then /I should (not )?see these ratings: (.*)/ do |uncheck, rating_list|
+  ratings_array = rating_list.split(/,\s+/)
+  Movie.find_all_by_rating(ratings_array).each do |movie|
+    if (!uncheck)  
+      page.body.should match(/#{movie.title}/m)
+    else 
+      page.body.should_not match(/#{movie.title}/m)
+    end
+  end
+end 
+    
